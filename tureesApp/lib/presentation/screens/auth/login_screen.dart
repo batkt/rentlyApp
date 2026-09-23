8eda8bf05c7080692fe5141a1921bc2ad829a17d
buynaaaa24 <abuynaa1211@gmail.com>
2026-09-23 09:15:02 +0800

    a

--- tureesApp/lib/presentation/screens/auth/login_screen.dart
+++ tureesApp/lib/presentation/screens/auth/login_screen.dart
@@ -990,7 +990,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen>
         ),
         const SizedBox(height: 4),
         Text(
-          'v3.0.7',
+          'v3.0.8',
           style: TextStyle(
             fontSize: 11,
             color: isDark ? const Color(0xFF334155) : AppColors.textTertiary,
